706f04de393f869c7762109e631578f87b8dc08b
ndhien <ndhien23@clc.fitus/edu/vn>
2026-04-20 09:42:43 +0700

    update train.sh

--- train.sh
+++ train.sh
@@ -3,6 +3,8 @@
 # Dừng script ngay lập tức nếu có bất kỳ lệnh nào bị lỗi
 set -e
 
+export CUDA_VISIBLE_DEVICES=0
+
 echo -e "\n1. Preprocessing..."
 python scripts/preprocess_data.py --num_train_samples 5390 --num_test_samples 1540
 
